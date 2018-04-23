e13481bf191e9eae5930537fea4957c9132c469e
DavidPrades <davidprades8@gmail.com>
2018-04-23 18:55:55 +0200

    Creating classes

---
+++ src/DirectionType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alu20908719v
+ */
+public enum DirectionType {
+    
+   
+    LEFT, RIGHT, UP, DOWN
+    
+}
---
+++ src/Node.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alu20908719v
+ */
+public class Node {
+    
+    private int row;
+    private int col;
+    
+    public Node(int row, int col){
+        
+        this.row=row;
+        this.col=col;
+        
+    }
+    
+    
+}
